baddeb8ac739976c3efd7fde5c35f83cee579a20
Gaspar Rodriguez <gaspar.unlp@gmail.com>
2021-06-08 09:41:07 +0200

    Change ignored files in gitignore

--- App.tsx
+++ App.tsx
@@ -3,7 +3,6 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {Navigation} from './src/navigation/Navigation';
-// import {FadeScreen} from './src/screens/FadeScreen';
 import {GradientProvider} from './src/context/GradientContext';
 
 const AppState = ({children}: any) => {
@@ -15,7 +14,6 @@ const App = () => {
     <NavigationContainer>
       <AppState>
         <Navigation />
-        {/* <FadeScreen /> */}
       </AppState>
     </NavigationContainer>
   );
