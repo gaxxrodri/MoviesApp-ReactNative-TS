05ef51375c40f75cf1ed91fb14b53016af75bb34
GaxRodri <76969924+gaxxrodri@users.noreply.github.com>
2021-06-08 02:03:01 +0200

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -7,15 +7,12 @@ import { FadeScreen } from './src/screens/FadeScreen';
 import { GradientProvider } from './src/context/GradientContext';
 
 const AppState = ({ children }: any) => {
-
   return (
     <GradientProvider>
       { children }
     </GradientProvider>
   )
-
 }
-
 
 const App = () => {
   return (
